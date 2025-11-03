c915dc3ecc7780aafddb1684506ff8252adaad5b
Paola Cardozo <cardozopaola328@gmail.com>
2025-11-03 00:29:53 -0300

    primer comic AramayoCardozo

---
+++ tpfinal/src/test/java/ar/ies6/tpfinal/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.ies6.tpfinal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
